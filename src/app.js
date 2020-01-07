17076a79dae8551762bc11c98f3389b012f4f26d
Luis Rivero <luis.arh.1109@gmail.com>
2020-01-06 22:31:01 -0400

    add cors

--- src/app.js
+++ src/app.js
@@ -3,10 +3,15 @@ const app = express();
 const authRouter = require('./controllers/authController')
 const peliculasRouter = require('./controllers/peliculaController');
 const categoriasRouter = require('./controllers/categoriaController');
+const cors = require('cors');
 
 // Settings
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cors({
+  origin: 'http://localhost:4200',
+  methods: 'GET,POST,PUT,DELETE'
+}))
 
 // Routers
 app.use(authRouter);
